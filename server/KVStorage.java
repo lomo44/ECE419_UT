a730a758db2bc5c600ac9b12bde59d2f8c670405
Zhuang Li <johnnn.li@mail.utoronto.ca>
2018-01-13 16:10:40 -0500

    - Interface for storage

---
+++ server/KVStorage.java
@@ -0,0 +1,15 @@
+package server
+
+import app_KvServer.IKVServer
+
+public class KVStorageAttribute{
+    CacheStrategy cacheType;
+}
+
+public class KVStorage {
+    public KVStorage(KVStorageAttribute in_Attribute){
+
+    }
+    public KVMessage process(KVMessage in_Message){
+    }    
+}
