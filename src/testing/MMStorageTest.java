bfb0ec354faa6fabafa250036ca8086af0377157
lomo44 <vamj44@gmail.com>
2018-01-23 17:38:03 -0500

    - Added proper mmstorage test

--- src/testing/MMStorageTest.java
+++ src/testing/MMStorageTest.java
@@ -4,26 +4,22 @@ import static org.junit.Assert.*;
 
 import java.io.IOException;
 
+import junit.framework.TestCase;
 import org.junit.Test;
 
 import database.storage.KVStorage;
 import database.storage.MMStorage;
 
-public class MMStorageTest {
+public class MMStorageTest extends TestCase{
 
 	@Test
-	public void test() {
-	 try {
+	public void test() throws Exception {
 		KVStorage store = new MMStorage(2000);
 		store.putToStorage("123", "123");
 		store.putToStorage("456", "456");
 		store.putToStorage("789", "789");
-		store.getFromStorage("123");
-		//((MMStorage) store).test();
-	} catch (Exception e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
+		assertEquals(store.getFromStorage("123"),"123");
+		store.clearStorage();
 	}
 
 }
